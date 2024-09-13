29331d84748d0321f52c4ab23770b654f4798b12
Vitalii <149200949+Velykobrat@users.noreply.github.com>
2024-09-13 17:58:55 +0300

    Крок 2
    Описані функції:
    - readContacts
    - writeContacts

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    const data = JSON.stringify(updatedContacts, null, 2); // Форматуємо JSON для зручності читання
+    await fs.writeFile(PATH_DB, data, 'utf-8');
+    console.log('Contacts successfully updated');
+  } catch (error) {
+    console.error('Error writing contacts:', error);
+    throw new Error('Could not write contacts data');
+  }
+};
